9cb380ee1c2c1a83f665efaabed1f9973a9ecb66
Tiffany Robinson <trobinson@CareKinesiss-MacBook-Pro-5.local>
2015-10-12 09:26:36 -0400

    some code refactoring

--- gameRun/gameRun.service.js
+++ gameRun/gameRun.service.js
@@ -4,44 +4,8 @@
     .module('run')
     .factory('runService', function ($http, $rootScope, $q) {
 
-          //Game Execution Functions
+          return msRun($q);
 
-          var determineMines = function(location, mines){
-             var deferred = $q.defer();
-              var row = Number(location[0]); 
-              var col = location[1];
-
-             var closeMines = discoverMineNeighbors(row, col, mines);
-
-             deferred.resolve(closeMines);
-             return deferred.promise;
-          }
-
-          var discoverMineNeighbors = function(row, col, gameData){
-           return _.filter( getNeighbors(row, col), function(aNeighbor){
-                      if(_.isUndefined(gameData[aNeighbor])){
-                        return;
-                      }
-                      return gameData[aNeighbor] === -1; 
-                  })
-          }
-
-          var getNeighbors = function(row, col){
-            return  [ ((row - 1) + col), 
-                      ((row + 1) + col), 
-                      ((row - 1) + String.fromCharCode(col.charCodeAt(0) - 1)), 
-                      ((row - 1) + String.fromCharCode(col.charCodeAt(0) + 1)),
-                      (row + String.fromCharCode(col.charCodeAt(0) - 1)),
-                      (row + String.fromCharCode(col.charCodeAt(0) + 1)),
-                      ((row + 1) + String.fromCharCode(col.charCodeAt(0) - 1)),
-                      ((row + 1) + String.fromCharCode(col.charCodeAt(0) + 1))
-                    ]
-          }
-
-          return {
-            determineMines: determineMines, 
-            getNeighbors: getNeighbors
-          };
      });
 
 }());
