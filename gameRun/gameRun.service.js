d82792402427c2b3d32e0112548a0e005842815e
Tiffany Robinson <trobinson@CareKinesiss-MacBook-Pro-5.local>
2015-10-13 13:31:47 -0400

    factoring out

--- gameRun/gameRun.service.js
+++ gameRun/gameRun.service.js
@@ -4,42 +4,44 @@
     .module('run')
     .factory('runService', function ($http, $rootScope, $q) {
 
-           var determineMines = function(location, mines){
-             var deferred = $q.defer();
-              var row = Number(location[0]); 
-              var col = location[1];
+      return mineRun($q);
 
-             var closeMines = discoverMineNeighbors(row, col, mines);
+          //  var determineMines = function(location, mines){
+          //    var deferred = $q.defer();
+          //     var row = Number(location[0]); 
+          //     var col = location[1];
 
-             deferred.resolve(closeMines);
-             return deferred.promise;
-          }
+          //    var closeMines = discoverMineNeighbors(row, col, mines);
 
-          var discoverMineNeighbors = function(row, col, gameData){
-           return _.filter( getNeighbors(row, col), function(aNeighbor){
-                      if(_.isUndefined(gameData[aNeighbor])){
-                        return;
-                      }
-                      return gameData[aNeighbor] === -1; 
-                  })
-          }
+          //    deferred.resolve(closeMines);
+          //    return deferred.promise;
+          // }
 
-          var getNeighbors = function(row, col){
-            return  [ ((row - 1) + col), 
-                      ((row + 1) + col), 
-                      ((row - 1) + String.fromCharCode(col.charCodeAt(0) - 1)), 
-                      ((row - 1) + String.fromCharCode(col.charCodeAt(0) + 1)),
-                      (row + String.fromCharCode(col.charCodeAt(0) - 1)),
-                      (row + String.fromCharCode(col.charCodeAt(0) + 1)),
-                      ((row + 1) + String.fromCharCode(col.charCodeAt(0) - 1)),
-                      ((row + 1) + String.fromCharCode(col.charCodeAt(0) + 1))
-                    ]
-          }
+          // var discoverMineNeighbors = function(row, col, gameData){
+          //  return _.filter( getNeighbors(row, col), function(aNeighbor){
+          //             if(_.isUndefined(gameData[aNeighbor])){
+          //               return;
+          //             }
+          //             return gameData[aNeighbor] === -1; 
+          //         })
+          // }
 
-          return {
-            determineMines: determineMines, 
-            getNeighbors: getNeighbors
-          };
+          // var getNeighbors = function(row, col){
+          //   return  [ ((row - 1) + col), 
+          //             ((row + 1) + col), 
+          //             ((row - 1) + String.fromCharCode(col.charCodeAt(0) - 1)), 
+          //             ((row - 1) + String.fromCharCode(col.charCodeAt(0) + 1)),
+          //             (row + String.fromCharCode(col.charCodeAt(0) - 1)),
+          //             (row + String.fromCharCode(col.charCodeAt(0) + 1)),
+          //             ((row + 1) + String.fromCharCode(col.charCodeAt(0) - 1)),
+          //             ((row + 1) + String.fromCharCode(col.charCodeAt(0) + 1))
+          //           ]
+          // }
+
+          // return {
+          //   determineMines: determineMines, 
+          //   getNeighbors: getNeighbors
+          // };
 
      });
 
